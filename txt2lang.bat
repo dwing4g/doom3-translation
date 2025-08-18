fef96a4bf550a3583aa509044c58977930fd228f
dwing <dwing4g@gmail.com>
2025-08-18 20:42:54 +0800

    many fixes for base.txt

--- txt2lang.bat
+++ txt2lang.bat
@@ -8,4 +8,6 @@ luajit txt2lang.lua base.txt base/strings/english.lang
 luajit txt2lang.lua bfa.txt  base/strings/english_bfa.lang
 luajit txt2lang.lua cc.txt   base/strings/english_cc.lang
 
+rem luajit lang2txt.lua E:\doom3bfg\base\strings\english.lang.ori base\strings\english.lang base.txt
+
 pause
